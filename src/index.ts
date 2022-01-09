316e7a7963a62c49ff3eece0b2fab29c98c12389
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-09 16:57:09 +0100

    refactor: split code to directories

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-import Fastify from 'fastify'
-import consola from 'consola'
+import { startHttpInterface } from './interfaces/http'
 
 async function main() {
-  const fastify = Fastify()
-
-  fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
-
-  const start = async () => {
-    try {
-      await fastify.listen(3000)
-      consola.success('Application running at http://localhost:3000')
-    } catch (err) {
-      consola.error(err)
-      process.exit(1)
-    }
-  }
-  start()
+  startHttpInterface()
 }
 
 main()
---
+++ src/interfaces/http.ts
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+import consola from 'consola'
+
+const fastify = Fastify()
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+export const startHttpInterface = async () => {
+  try {
+    await fastify.listen(3000)
+    consola.success('Application running at http://localhost:3000')
+  } catch (err) {
+    consola.error(err)
+    process.exit(1)
+  }
+}
