cc7d95f64766875a558712238d4b9d03498fc74f
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-09 01:33:41 +0100

    chore: add fastify to work with single package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import Fastify from 'fastify'
+
 async function main() {
-    console.log("Hello World!");
+  const fastify = Fastify()
+  fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
+  const start = async () => {
+    try {
+      await fastify.listen(3000)
+      console.log('Application running at http://localhost:3000')
+    } catch (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  }
+  start()
 }
 
-main();
+main()
