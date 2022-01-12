b6f8e67879faa15af51607865a69e2cb6956f0bf
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-12 17:57:10 +0100

    chore: add realm

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
-// import { HTTPInteface } from './interfaces/http'
+import { HTTPInteface } from './interfaces/http'
 import bcrypt from 'bcrypt'
+import realm from 'realm'
 import path from 'path'
 
 require('bcrypt/lib/binding/napi-v3/bcrypt_lib.node')
 
-export function main() {
+export interface User extends realm.Object {
+	id: string
+}
+
+export const UserSchema: realm.ObjectSchema = {
+	name: 'User',
+	properties: {
+		id: {
+			type: 'string',
+			optional: false,
+		},
+	},
+	primaryKey: 'id',
+}
+
+export async function main() {
 	const salt = bcrypt.genSaltSync(5)
 	const hash = bcrypt.hashSync('password', salt)
 	console.log(hash)
+
+	const r = await realm.open({
+		path: path.join(__dirname, 'db', 'db.realm'),
+		schema: [UserSchema],
+	})
+
+	r.close()
+
+	new HTTPInteface().startup()
 	return hash
 }
 
 main()
-
-// new HTTPInteface().startup()
