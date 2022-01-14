bfc9433172445c5df28eff083ab5abab416f8814
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-14 04:09:33 +0100

    build(pkg): add direct require of native node code

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { HTTPInteface } from './interfaces/http'
 import { hash } from './builds/bcrypt'
 // import { relmio } from './builds/realm'
 
+/* Explict require of .node binary
+https://github.com/vercel/pkg/issues/1473 */
+
+require('bcrypt/lib/binding/napi-v3/bcrypt_lib.node')
+
 export async function main() {
 	hash('password')
 	// relmio()
