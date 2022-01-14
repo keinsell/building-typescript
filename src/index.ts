076572cd798825369ea0c98ac6fdba846d650dd8
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-14 06:38:19 +0100

    build: disable sqlite3 and check if pkg.assets work

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { hash } from './builds/bcrypt'
-import { slite } from './builds/sqlite'
+// import { slite } from './builds/sqlite'
 // import { relmio } from './builds/realm'
 
 /* I cannot into pkg.assets configuration */
-require('bcrypt/lib/binding/napi-v3/bcrypt_lib.node')
-require('sqlite3/lib/binding/**/node_sqlite3.node')
+// require('bcrypt/lib/binding/napi-v3/bcrypt_lib.node')
+// require('sqlite3/lib/binding/**/node_sqlite3.node') // Doesn't work at all
 
 export async function main() {
 	hash('password')
-	slite()
+	// slite()
 }
 
 main()
