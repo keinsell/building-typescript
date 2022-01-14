1ae6bbceafe23e7383ab831caf8f7d629a92ae95
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-14 05:22:44 +0100

    chore: disable realm from build

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { HTTPInteface } from './interfaces/http'
 
 import { hash } from './builds/bcrypt'
-import { relmio } from './builds/realm'
+// import { relmio } from './builds/realm'
 
 /* Explict require of .node binary
 https://github.com/vercel/pkg/issues/1473 */
 
 require('bcrypt/lib/binding/napi-v3/bcrypt_lib.node')
-
-// Realm is weird but let's try it in this way for caxa build
-
-require('realm/lib/submit-analytics.js')
+// require('realm/lib/submit-analytics.js')
 
 export async function main() {
 	hash('password')
-	relmio()
+	// relmio()
 	// new HTTPInteface().startup()
 }
 
