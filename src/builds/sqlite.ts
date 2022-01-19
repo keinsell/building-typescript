5a28c604adbf2f129756b5f46f5ff27e986c6837
Jay Whitewood <jakub.jay.olan@gmail.com>
2022-01-19 04:56:05 +0100

    fix: use `path.resolve` instead `path.join`

--- src/builds/sqlite.ts
+++ src/builds/sqlite.ts
@@ -1,9 +1,10 @@
 import { open } from 'sqlite'
 import sqlite from 'sqlite3'
+import path from 'path'
 
 export async function slite() {
 	const database = await open({
-		filename: 'database.db',
+		filename: path.resolve(__dirname, 'database.db'),
 		driver: sqlite.Database,
 	})
 	return database
